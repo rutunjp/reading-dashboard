212de485c665f409dec1665549f1960056f7a3a9
Rutunj <rutunjp206@gmail.com>
2023-03-25 23:27:09 +0530

    add BooksDashboard to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,17 +7,16 @@ import {
   Button,
   HStack,
 } from '@chakra-ui/react'
-
+import BooksDashboard from '@/components/booksDashboard'
 export default function Home() {
   return (
     <ChakraProvider>
       <HStack align={'start'}>
         <Sidebar />
-        <Container
-          maxW={'100%'}
-          minH={'100vh'}
-          backgroundColor="#f5f5f5"
-        ></Container>
+
+        <Container maxW={'100%'} minH={'100vh'} backgroundColor="#f5f5f5">
+          <BooksDashboard />
+        </Container>
       </HStack>
     </ChakraProvider>
   )
